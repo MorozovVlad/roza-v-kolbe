1a0c5c4e2c95cf79ef36e48df3e6e5dcfed52e7f
Vladislav Morozov <m_uladzislau2000@mail.ru>
2023-12-02 21:40:10 +0300

    fix

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php 
 
 // require_once('phpmailer/PHPMailerAutoload.php');
-require_once('./phpmailer/PHPMailerAutoload.php');
+require_once('../phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
