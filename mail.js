e39da8481014c51dc3c5e603cdac75a05cdd0b1e
Morozov Vlad <m.uladzislau.m@gmail.com>
2023-12-06 10:36:08 +0300

    fix

--- mail.js
+++ mail.js
@@ -1,21 +1,22 @@
 function SendEmail() {
   let name = document.getElementById("name").value;
   let tel = document.getElementById("tel").value;
+  if (name && tel) {
+    let body =
+      "Новая заявка на розу в колбе. <br/> Имя: " +
+      name +
+      "<br/> Телефон: " +
+      tel;
+    let subject = "Заявка на розу в колбе от " + name;
 
-  let body =
-    "Новая заявка на розу в колбе. <br/> Имя: " +
-    name +
-    "<br/> Телефон: " +
-    tel;
-  let subject = "Заявка на розу в колбе от " + name;
-
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "ip.muhlyado.maxim@gmail.com",
-    Password: "96AC4554E4BAB70E1A17FE751910A6C39BA0",
-    To: "ip.muhlyado.maxim@gmail.com",
-    From: "ip.muhlyado.maxim@gmail.com",
-    Subject: subject,
-    Body: body,
-  }).then((message) => alert("Ваш заказ принят! Ожидайте звонка"));
+    Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "ip.muhlyado.maxim@gmail.com",
+      Password: "96AC4554E4BAB70E1A17FE751910A6C39BA0",
+      To: "ip.muhlyado.maxim@gmail.com",
+      From: "ip.muhlyado.maxim@gmail.com",
+      Subject: subject,
+      Body: body,
+    }).then((message) => alert("Ваш заказ принят! Ожидайте звонка"));
+  }
 }
